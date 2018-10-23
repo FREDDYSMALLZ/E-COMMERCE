d2da515b8c003c750071c20bf48b981af9abbbcf
fredi <fredieondieki@outlook.com>
2018-10-23 10:55:07 -0500

    Added functionality for new user to register and login

--- Includes/Front/Side_Nav.php
+++ Includes/Front/Side_Nav.php
@@ -1,20 +1,17 @@
-<?php
-include_once 'Includes/dbh.inc.php';
-?>
+<?php include_once('server.php') ?>
+<?php include_once('db.php') ?>
 <div class="col-md-3">
     <p class="lead">Boutique</p>
     <div class="list-group">
 
         <?php
-        $sql = "SELECT * FROM categories;";
-        $result = mysqli_query($conn, $sql);
+        $sql = "SELECT * FROM users;";
+        $result = mysqli_query($db, $sql);
         $resultCheck = mysqli_num_rows( $result);
-
         if ( $resultCheck > 0){
-
             while ($row = mysqli_fetch_assoc($result)){
+                echo "<a href='' class='list-group-item'>{$row['FirstName']}</a>";
 
-                echo "<a href='' class='list-group-item'>{$row['category_title']}</a>";
             }
         }
         ?>
--- login.php
+++ login.php
@@ -1,85 +1,53 @@
+
+<!--
+The Server.php file is included serves the purpose of connecting the database when the user logs into
+the website.
+-->
+<?php include('server.php') ?>
+<?php include('db.php') ?>
+
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Login Page - E-commerce</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="Includes/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="Includes/css/shop-homepage.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+	<title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<style>
+	body{
+		margin: 0;
+		background:url('Images/design.jpg');
+		background-size: cover;
+	}
+	</style>
 </head>
-
 <body>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <?php include("Includes/Front/Top_NavBar.php");?>
-        <!-- /.container -->
-    </nav>
 
-    <!-- Page Content -->
-    <div class="container">
+	<div class="header">
+		<h2>The E-Commerce Login Page </h2>
+	</div>
 
-      <header>
-            <h1 class="text-center">La-Boutique Login form</h1>
-        <div class="col-sm-4 col-sm-offset-5">         
-            <form class="" action="" method="post" enctype="multipart/form-data">
-                <div class="form-group"><label for="">
-                    Username<input type="text" name="username" class="form-control"></label>
-                </div>
-                 <div class="form-group"><label for="password">
-                    Password<input type="text" name="password" class="form-control"></label>
-                </div>
+	<form method="post" action="login.php">
+		<!-- Get the errors incase the user attempts to log in with empty fields -->
+		<?php include('errors.php'); ?>
 
-                <div class="form-group">
-                  <input type="submit" name="submit" class="btn btn-primary" >
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user">Login</button>
+		</div>
 
-                    <p style="font-size:18px; color:#020607">
-                        New Customer? <a href="register.php" style="color: #3c5adb;">Sign Up</a>
-                    </p>
-                </div>
-            </form>
-        </div>
+		<p style="font-size:18px; color:#c1c9c7">
+			New Customer ? <a href="registration.php" style="color: #2bc0db;">Sign up</a>
 
-    </header>
+		</p>
 
-        </div>
-
-    </div>
-    <!-- /.container -->
-
-    <div class="container">
-
-        <hr>
-
-        <!-- Footer -->
-        <?php include("Includes/Back/Footer.php");?>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="Includes/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="Includes/js/bootstrap.min.js"></script>
+	</form>
 
 </body>
-
 </html>
