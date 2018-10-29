90a991c5fbbabbfc89a0dbbeec45948f5d0008e6
fredi <fredieondieki@outlook.com>
2018-10-29 02:20:15 -0500

    Refactored the side nav files to getcategories file for clean code

--- Includes/Front/Side_Nav.php
+++ Includes/Front/Side_Nav.php
@@ -2,18 +2,6 @@
 <?php include_once('db.php') ?>
 <div class="col-md-3">
     <p class="lead">Boutique</p>
-    <div class="list-group">
 
-        <?php
-        $sql = "SELECT * FROM categories;";
-        $result = mysqli_query($db, $sql);
-        $resultCheck = mysqli_num_rows( $result);
-        if ( $resultCheck > 0){
-            while ($row = mysqli_fetch_assoc($result)){
-                echo "<a href='' class='list-group-item'>{$row['categories_title']}</a>";
-
-            }
-        }
-        ?>
-    </div>
+    <?php include("Includes/getcategories.php");?>
 </div>
