9c55f0b44f3dc3b91c1f0426b2c5faf7785f5ba3
fredi <fredieondieki@outlook.com>
2018-11-11 22:59:59 -0600

    Reflect on changes

--- Includes/Front/Header.php
+++ Includes/Front/Header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Shop Homepage - Start Bootstrap Template</title>
+    <title>E-Commerce Shop</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="Includes/css/bootstrap.min.css" rel="stylesheet">
